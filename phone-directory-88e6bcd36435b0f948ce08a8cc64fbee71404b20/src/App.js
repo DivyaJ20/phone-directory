60a2280767fd9399efdac4eea1dce89c4782b2a9
DivyaJ20 <62278950+DivyaJ20@users.noreply.github.com>
2020-06-13 20:57:06 +0530

    created skeleton of home page

--- phone-directory-88e6bcd36435b0f948ce08a8cc64fbee71404b20/src/App.js
+++ phone-directory-88e6bcd36435b0f948ce08a8cc64fbee71404b20/src/App.js
@@ -4,7 +4,16 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div >
+      <div>
+         Phone Directory
+        <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
       </div>
     );
   }
